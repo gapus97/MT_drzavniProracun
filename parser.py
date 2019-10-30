eca0703829c80a8766d22ec4ff156ff5c4b52848
mkloar <miha.kloar@gmail.com>
2019-10-30 20:03:38 +0100

    Push data to elasticsearch DB

--- parser.py
+++ parser.py
@@ -1,23 +1,28 @@
 import csv
-from itertools import groupby
 import pandas as pd
-import matplotlib.pyplot as plt
-
+from elasticsearch import Elasticsearch
+es = Elasticsearch(
+    ['localhost'],
+    port=9200
+)
 
 file = "reb2019spl.csv"
 data = pd.read_csv(file, encoding="utf-16", header=0, engine="python", sep='\t')
 
-# parse data as dataframe
-df = pd.DataFrame(data)
-df['Rebalans proračuna 2019'] = df['Rebalans proračuna 2019'].str.replace(",",".")
+with open(file, mode='r', encoding='utf-16') as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter='\t')
+    line_count = 0
+    for row in csv_reader:
+        # delete unnecessary columns
+        del row['K4_ID']
+        del row['K3_ID']
+        del row['K2_ID']
+        del row['BLC_ID']
 
-# group data and aggregate sum
-groupedData = df.groupby(['BLC_NAME','K2_NAME', 'K3_NAME' , 'K4_NAME']).agg({"Rebalans proračuna 2019": "sum"})
+        #make column in millions
+        row['Rebalans proračuna 2019'] = pd.to_numeric(row['Rebalans proračuna 2019'].replace(",", ".")) / 1000000
 
+        # push data into elasticsearch
+        es.index(index='proracun', body=row)
+        line_count += 1
 
-groupedData["Rebalans proračuna 2019"] = pd.to_numeric(groupedData["Rebalans proračuna 2019"].replace(",","."))
-
-pd.pivot_table(groupedData, values = 'Rebalans proračuna 2019', index = 'BLC_NAME',
-              columns = 'BLC_NAME',aggfunc ='count').plot.bar()
-
-plt.show()
