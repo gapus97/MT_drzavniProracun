18d7dfcd31b1b34e51d510aac8d6937a2f0141f2
GodZilla <miha.kloar@gmail.com>
2019-11-02 17:49:06 +0100

    Update main part of application, update CSS

--- app/src/App.js
+++ app/src/App.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import './App.css';
-//import 'leaflet/dist/leaflet.css';
-import States from './components/States';
-import BarChart from './components/charts/BarChart';
-import Main from './sites/Main';
-
+import Router from './Router';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Header</p>
-        <States />
-        <BarChart />
-      </header>
-
-      <Main />
-
-      <footer className="footer">
-        <div>
-          <p>This app uses data from <a href="https://www.e-prostor.gov.si/">e-prostor gov </a></p>
-        </div>
-      </footer>
+      <Router />
     </div>
   );
 }
