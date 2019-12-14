7bb879d468f953db0f773f1f7d53f27882992be2
mkloar <miha.kloar@gmail.com>
2019-12-14 09:46:49 +0100

    [Parsers] update parser for states

--- dataParsers/parseStates.py
+++ dataParsers/parseStates.py
@@ -1,6 +1,7 @@
 from pyproj import Proj
 from dbfread import DBF
 from elasticsearch import Elasticsearch
+import csv
 
 es = Elasticsearch(
     ['localhost'],
@@ -15,6 +16,7 @@ myProj = Proj("+proj=longlat +datum=WGS84 +to +proj=tmerc +lon_0=15E +ellps=GRS8
 
 
 id = 0
+states = []
 for record in DBF(file, encoding="cp1250"):
     # state name
     name = record['OB_UIME']
@@ -32,7 +34,30 @@ for record in DBF(file, encoding="cp1250"):
         "lat": lat
     }
 
+    states.append(obj)
     # push data to elastic
-    es.index(index='states', doc_type='Blog', id=id, body=obj)
+    #es.index(index='states', doc_type='Blog', id=id, body=obj)
     id += 1
+
+
+with open("statesPopulation.csv", encoding="cp1250") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            line_count += 1
+            continue
+
+        if len(row) > 2:
+            state_name = row[1]
+            indexOfSub = state_name.find("/")
+            state_name = state_name[:indexOfSub]
+            population = row[2]
+            for state in states:
+                if state_name in state["name"]:
+                    index = states.index(state)
+                    state["population"] = int(population)
+                    states[index] = state
+                    es.index(index='states', doc_type='Blog', id=state["id"], body=state)
+            line_count += 1
 
