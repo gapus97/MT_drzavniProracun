5ae6f0e593c2c1f9d2fda0847ff37bd02655494e
mkloar <miha.kloar@gmail.com>
2019-12-15 16:01:43 +0100

    Update router api

--- api/router.js
+++ api/router.js
@@ -1,6 +1,6 @@
 'use strict';
 const { Client } = require('@elastic/elasticsearch');
-const client = new Client({ node: 'http://0.0.0.0:9200' });
+const client = new Client({ node: 'http://localhost:9200' });
 const express = require('express');
 const router = new express.Router();
 
@@ -74,7 +74,7 @@ const parseAllCategories = async (stateName) => {
                         "categorie": catName,
                         "data": item
                     });
-                    console.log(overallBudgetData);
+                    //console.log(overallBudgetData);
                     
                 }
             } else {
