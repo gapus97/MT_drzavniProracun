884f86c7d940f93a3421b7cfa813d4a51691dfd9
gapus97 <gaspersmerkolj@gmail.com>
2019-12-18 23:21:49 +0100

    [App] style additional information and add a slider

--- app/src/components/StateIndexDataShower.js
+++ app/src/components/StateIndexDataShower.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
-
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 class StateIndexDataShower extends React.Component {
 
@@ -31,9 +31,9 @@ class StateIndexDataShower extends React.Component {
                 <Row>
                     {Object.keys(this.state.additionalInfo.values[0]).map((state, i) => {
                         console.log(state);
-                        return <Col>
-                            <p>Kategorija: {state}</p>
-                            <p>Vrednost: { this.state.additionalInfo.values[0][state] }</p>
+                        return <Col class={"col-md-offset-2"} style={{backgroundColor:'#282D33',margin:'0 2.5%',width:'20%', marginBottom:'20px'}}>
+                            <p style={{color:'white'}}>Kategorija: {state}</p>
+                            <p style={{color:'white'}}>Vrednost: { Number((this.state.additionalInfo.values[0][state]).toFixed(2))}</p>
                         </Col> 
                     })
 
@@ -51,18 +51,19 @@ class StateIndexDataShower extends React.Component {
 
 
         return (
-            <Container>
+            <Container >
                 <Container>
                     <p>Predlagane občine</p>
                 </Container>
-                <Row>
+                <Carousel showStatus={false} showIndicators={false}>
                   {this.props.data.map((state, i) => (
-                      <Col key={i} className="indeks-item" onClick={e => this.onColumnClick(e, state)}>
+                      <div key={i} className="indeks-item" onClick={e => this.onColumnClick(e, state)}>
                           <p>{state.name}</p>
-                          <p>{state.normalisedIndex}</p>
-                      </Col>
+                          <p>{Number((state.normalisedIndex).toFixed(1))}</p>
+                          <img src="" class="d-block w-100" ></img>
+                      </div>
                   ))}
-                </Row>
+                </Carousel>
 
                 {
                     additionalInfo && showAdditionalInfo ?
