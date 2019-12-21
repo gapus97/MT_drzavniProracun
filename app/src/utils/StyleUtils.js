44211ff378123bf98aa63b274c1c99535f7d278e
mkloar <miha.kloar@gmail.com>
2019-12-21 18:16:26 +0100

    [APP] add dark theme

---
+++ app/src/utils/StyleUtils.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const darkTheme = {
+    body: '#363537',
+    text: '#FAFAFA',
+    toggleBorder: '#6B8096',
+    gradient: 'linear-gradient(#091236, #1E215D)',
+};
+
+export const GlobalStyles = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+
+  body {
+    align-items: center;
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+    /*display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100vh;*/
+    margin: 0;
+    padding: 0;
+    font-family: BlinkMacSystemFont, -apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
+    transition: all 0.25s linear;
+    }`;
