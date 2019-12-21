51827f3aaa26fea4c0bc9be299cca3ebc6ee4ec5
mkloar <miha.kloar@gmail.com>
2019-12-21 18:17:21 +0100

    [APP] update site with dark theme

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Routes from './Router';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, GlobalStyles } from './utils/StyleUtils';
 
 function App() {
   return (
-    <div className="App">
-      <Routes />
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyles />
+        <div className="App">
+          <Routes />
+        </div>
+    </ThemeProvider>
   );
 }
 
